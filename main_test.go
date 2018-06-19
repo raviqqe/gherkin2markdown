17c6d64d6992ba3d09a5fbfefdc5081d29b24dc4
Yota Toyama <raviqqe@gmail.com>
2018-06-20 07:01:19 +0900

    Test command with directory

--- main_test.go
+++ main_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"io/ioutil"
 	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -21,4 +22,26 @@ func TestCommand(t *testing.T) {
 
 func TestCommandWithNonExistentFile(t *testing.T) {
 	assert.NotNil(t, command([]string{"non-existent.feature"}, ioutil.Discard))
+}
+
+func TestCommandWithDirectory(t *testing.T) {
+	r, err := ioutil.TempDir("", "")
+	assert.Nil(t, err)
+
+	s := filepath.Join(r, "src")
+	err = os.Mkdir(s, 0700)
+	assert.Nil(t, err)
+
+	err = ioutil.WriteFile(filepath.Join(s, "foo.feature"), []byte("Feature: Foo"), 0600)
+	assert.Nil(t, err)
+
+	d := filepath.Join(r, "dest")
+
+	assert.Nil(t, command([]string{s, d}, ioutil.Discard))
+
+	bs, err := ioutil.ReadFile(filepath.Join(d, "foo.md"))
+	assert.Nil(t, err)
+	assert.Equal(t, "# Foo\n", string(bs))
+
+	os.RemoveAll(r)
 }
