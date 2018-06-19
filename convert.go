a90d52f69789392317633bdd78930f0b3fbde9a5
Yota Toyama <raviqqe@gmail.com>
2018-06-20 06:33:04 +0900

    Implement convertFiles

--- convert.go
+++ convert.go
@@ -4,9 +4,14 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"path/filepath"
+	"strings"
+	"sync"
 
 	"github.com/cucumber/gherkin-go"
 )
+
+const featureFileExtension = ".feature"
 
 func convertFile(s string, w io.Writer) error {
 	f, err := os.Open(s)
@@ -27,5 +32,70 @@ func convertFile(s string, w io.Writer) error {
 }
 
 func convertFiles(s, d string) error {
-	panic("unimplemented")
+	ps := []string{}
+
+	err := filepath.Walk(s, func(p string, i os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if !i.IsDir() && filepath.Ext(p) == featureFileExtension {
+			ps = append(ps, p)
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		return err
+	}
+
+	w := sync.WaitGroup{}
+	es := make(chan error, len(ps))
+
+	for _, p := range ps {
+		w.Add(1)
+
+		go func(p string) {
+			defer w.Done()
+
+			f, err := openDestFile(p, s, d)
+
+			if err != nil {
+				es <- err
+				return
+			}
+
+			err = convertFile(p, f)
+
+			if err != nil {
+				es <- err
+				return
+			}
+		}(p)
+	}
+
+	w.Wait()
+
+	if len(es) != 0 {
+		return <-es
+	}
+
+	return nil
+}
+
+func openDestFile(p, s, d string) (*os.File, error) {
+	p, err := filepath.Rel(s, p)
+
+	if err != nil {
+		return nil, err
+	}
+
+	p = strings.TrimSuffix(filepath.Join(d, p), featureFileExtension) + ".md"
+
+	if err := os.MkdirAll(filepath.Dir(p), 0700); err != nil {
+		return nil, err
+	}
+
+	return os.OpenFile(p, os.O_CREATE|os.O_WRONLY, 0600)
 }
